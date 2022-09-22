c73b82c1aecd2f1fc76e76b901a5538a64d222f1
Khaled Ameen <Khaled.ameen11@gmail.com>
2022-09-22 14:06:04 +0200

    adding print PI

--- Untitled-1.py
+++ Untitled-1.py
@@ -7,7 +7,12 @@ import numpy
 """
 
 
+import math
+
+
 print("test")
 pi_squared = 5 * 2
+print("this is the magic PI",math.pi)
 print("this is the squred:", pi_squared)
+
 #this is a test to see how this thing work
