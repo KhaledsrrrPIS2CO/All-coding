506a6ebe9abbd913d55b803dd6225ea8bf61669c
Khaled Ameen <Khaled.ameen11@gmail.com>
2022-09-29 21:44:58 +0200

    AAPL CSV  up to date with 'origin/main'.

--- import pandas as pd.py
+++ import pandas as pd.py
@@ -4,7 +4,7 @@ import pandas as pd
 def test_run():
     """Function called by Test Run."""
     df = pd.read_csv("data/AAPL.csv")
-    print (df [0:6])
+    print (df [10:61]) # slicing 
 
 
 if __name__ == "__main__":
