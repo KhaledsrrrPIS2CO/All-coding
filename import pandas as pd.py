75c8e93047e6e1b77be9ba0ad6e8a1bdb7a84408
Khaled Ameen <Khaled.ameen11@gmail.com>
2022-09-29 21:37:30 +0200

    (Test run) reads AAPL.csv and saves to df+print

--- import pandas as pd.py
+++ import pandas as pd.py
@@ -1,37 +1,11 @@
 import pandas as pd
 
-aapl_price_surl= 'https://finance.yahoo.com/quote/AAPL/history?p=AAPL'
 
 def test_run():
-    #Function called by Test Run
-    df = pd.read_html("data/AAPL.csv")
-    df[10:21]
+    """Function called by Test Run."""
+    df = pd.read_csv("data/AAPL.csv")
+    print (df [0:6])
 
 
 if __name__ == "__main__":
     test_run()
-
-
-"""
-import pandas as pd
-import yfinance as yf
-
-aapl_url = 'https://finance.yahoo.com/quote/AAPL/history?p=AAPL'
-
-
-def get_max_close(symbol):
-    #retrun the max value for stock close by indicating the toicker
-    df = pd.read_csv("data{}.csv".format(symbol)) # read in data
-    return df['Close'].max() #compute the paramet of close  and return max function
-
-def test_run():
-    #function called byy test_run it loops over AAAPL & IBM and it will priunyt max value of both stocks   
-    for symbol in ['AAPL', "IBM"]:
-        print("Max close:")
-        print(symbol, "/see", get_max_close,)
-        
-if __name__ == "__main__":
-    test_run()
-
-"""
-
