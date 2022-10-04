0f94e9b98d81d047218582b27f6241e02858e051
Khaled Ameen <Khaled.ameen11@gmail.com>
2022-10-04 22:26:41 +0200

    TWTR + AAPL prices

--- import pandas as pd.py
+++ import pandas as pd.py
@@ -3,9 +3,13 @@ import pandas as pd
 
 def test_run():
     """Function called by Test Run."""
-    df = pd.read_csv("data/AAPL.csv")
-    print (df [10:61]) # slicing 
+    dfaapl = pd.read_csv("data/AAPL.csv")
+    print (dfaapl [0:5]) # slicing 
+    dfTWTR = pd.read_csv("data/TWTR.csv")
+    print(dfTWTR [0:5]) # slicing 
 
 
 if __name__ == "__main__":
     test_run()
+
+
