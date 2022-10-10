c640f19f6c47a48075bf8a388140b6c7987d761e
Khaled Ameen <Khaled.ameen11@gmail.com>
2022-10-10 22:27:31 +0200

    join df1 + SPY data

--- working with multiple stocks.py
+++ working with multiple stocks.py
@@ -35,3 +35,47 @@ def test_run():
 if __name__ == "__main__":
     test_run( )
 
+
+import csv
+import pandas as pd
+import matplotlib.pyplot as plt
+"""
+
+"""
+def get_mean_vol(symbol):
+    """retursn the mean vol of a certain stocks, takes volume
+    data is stored locallay all coding/data/<>symbol.csv
+    """
+    df = pd.read_csv("data/{}.csv".format(symbol)) #read in data
+
+
+def plot_stock_price():
+    df = pd.read_csv("data/AAPL.csv")
+    print (df['Adj Close'])
+    df['Adj Close'].plot()
+    plt.show() # it must be called to show plots
+
+if __name__ == "__main__":
+    plot_stock_price()
+
+#plotting AAPL stock price
+
+
+
+def plot_high_price():
+    df = pd.read_csv("data/AAPL.csv")
+    print (df['High'])
+    df['High'].plot()
+    plt.show()
+
+if __name__ == "__main__":
+    plot_high_price()
+
+
+def plot_close_n_adjClose():
+    df = pd.read_csv("data/AAPL.csv")
+    df[['Close', 'Adj Close']].plot()
+    plt.show()
+
+if __name__ == "__main__":
+        plot_close_n_adjClose()
