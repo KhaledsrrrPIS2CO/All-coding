301b30cb5456fe6b83e1b9d027e55914e20f94e8
Khaled Ameen <Khaled.ameen11@gmail.com>
2023-01-02 20:49:32 +0100

    Authetcation with iex API

--- Algo trading testing.py
+++ Algo trading testing.py
@@ -3,3 +3,21 @@ import pandas as pd
 import requests 
 import math
 import xlsxwriter
+
+print("!--------!")
+
+
+#import the list of stocks
+#the idea is to create equal weight  
+
+#now save the csv stock list as pandas dataframe 
+stcoks = pd.read_csv('sp_500_stocks.csv')
+
+
+# now aquiring the API
+#Authentifcation to pull data from the API 
+#Dummy data secrets.py, et ignore the info in secretrs py is not shared 
+#Sandbox API  
+
+from secrets import IEX_CLOUD_API_TOKEN
+
